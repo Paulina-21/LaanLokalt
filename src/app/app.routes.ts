08d8f4acbb746c17bfc6a89c4402494f02f2495e
Mark Svendstrup Knudsen <mark4610@edu.easj.dk>
2023-04-03 12:13:02 +0200

    create food module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,5 +10,9 @@ export const routes: Routes = [
     path: 'folder/:id',
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
+  },  {
+    path: 'food',
+    loadComponent: () => import('./pages/food/food.page').then( m => m.FoodPage)
   },
+
 ];
