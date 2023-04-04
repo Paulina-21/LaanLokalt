ec9bfd708eb73d849d3292f6d16afb93005366f9
Paula Catalina Curca <paul6795@edu.easj.dk>
2023-04-04 13:01:51 +0200

    User+Item Interface

---
+++ src/app/interfaces/item.ts
@@ -0,0 +1,23 @@
+export interface Item {
+    ItemId: number
+    Title : string
+    Description : string
+    Image: string
+    CreatedDate: Date
+    Type : Type
+    FilterTag: FilterTag
+    Adress : string
+    UserId: number 
+    Price: number
+}
+
+export enum FilterTag{
+    offered = 1,
+    wanted = 2,
+    myPosts = 3
+  }
+export enum Type {
+    food = 1,
+    tool = 2,
+    service = 3
+  }
