ca663e1009bf50a6f5f02b92e3d8d3bacf08639e
Mark Svendstrup Knudsen <mark4610@edu.easj.dk>
2023-04-05 16:20:19 +0200

    removed "{" that should not be there

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,21 +8,20 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-  {
     path: 'food',
     loadComponent: () => import('./pages/food/food.page').then(m => m.FoodPage)
   },
   {
     path: 'petsandplants',
-    loadComponent: () => import('./pages/pets-and-plants/pets-and-plants.page').then( m => m.PetsAndPlantsPage)
+    loadComponent: () => import('./pages/pets-and-plants/pets-and-plants.page').then(m => m.PetsAndPlantsPage)
   },
   {
     path: 'resources',
-    loadComponent: () => import('./pages/resources/resources.page').then( m => m.ResourcesPage)
+    loadComponent: () => import('./pages/resources/resources.page').then(m => m.ResourcesPage)
   },
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+    loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage)
   },
   { path: 'item', component: ItemComponent },
 ];
