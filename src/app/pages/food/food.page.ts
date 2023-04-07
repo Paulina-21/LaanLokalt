5a8a429eda75c6663c58e373c60682aaab5bc04f
Mark Svendstrup Knudsen <mark4610@edu.easj.dk>
2023-04-07 13:38:43 +0200

    something

--- src/app/pages/food/food.page.ts
+++ src/app/pages/food/food.page.ts
@@ -8,25 +8,26 @@ import { HeaderComponent } from 'src/app/components/header/header.component';
 
 @Component({
   selector: 'app-food',
-  templateUrl: './food.page.html',
-  styleUrls: ['./food.page.scss'],
+  templateUrl: 'food.page.html',
+  styleUrls: ['food.page.scss'],
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule, HeaderComponent]
 })
 
 export class FoodPage implements OnInit {
+
+  itemList: IItem[];
+
   constructor(
     private itemService: DatabaseService,
   ) { }
 
   ngOnInit() {
-    this.getItems();
+    this.itemService.getData().subscribe((data) => { this.itemList = data.itemList });
   }
 
-  itemList: IItem[];
-
-  getItems(): void {
+  /*getItems(): void {
     this.itemService.readJsonFile()
       .subscribe(items => this.itemList = items);
-  }
+  }*/
 }
--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import itemJson from './../data/items.json';
 import { IItem } from '../interfaces/item';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
   constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get<any>('./data/items.json');
+  }
+
   readJsonFile() {
-    return this.http.get<IItem[]>('/data/items.json'); 
-}
+    return this.http.get<IItem[]>('/data/items.json');
+  }
 }
