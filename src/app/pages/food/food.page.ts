ae4aa1a02a5cb5afa51989b4ba3a2b1a3cee2bac
mark-knudsen <marksvendstrupknudsen@gmail.com>
2023-04-07 16:30:16 +0200

    some

--- src/app/pages/food/food.page.ts
+++ src/app/pages/food/food.page.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 import { DatabaseService } from './../../services/database.service';
 import { IItem } from 'src/app/interfaces/item';
 import { HeaderComponent } from "../../components/header/header.component";
-import { RouteReuseStrategy } from '@angular/router';
 
 @Component({
     selector: 'app-food',
-    templateUrl: './food.page.html',
-    styleUrls: ['./food.page.scss'],
+    templateUrl: 'food.page.html',
+    styleUrls: ['food.page.scss'],
     standalone: true,
     imports: [IonicModule, CommonModule, FormsModule, HeaderComponent],
-    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
 })
 
 export class FoodPage implements OnInit {
@@ -22,13 +20,12 @@ export class FoodPage implements OnInit {
   constructor(private itemService: DatabaseService){ }
 
   ngOnInit() {
-    this.itemService.getData().subscribe((data) => {
-      this.itemList = data.itemList;
-    });
+   
+    this.getItems();
   } 
-/*
+
   getItems(): void {
     this.itemService.readJsonFile()
       .subscribe(items => this.itemList = items);
-  }*/
+  }
 }
--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import itemJson from './../data/items.json';
+import itemJson from '../../assets/data/items.json';
 import { IItem } from '../interfaces/item';
 import { Observable } from 'rxjs';
 
@@ -10,11 +10,11 @@ import { Observable } from 'rxjs';
 export class DatabaseService {
   constructor(private http: HttpClient) { }
   
-  /*
+  
   readJsonFile() {
-    return this.http.get<IItem[]>('/data/items.json'); 
-  }*/
+    return this.http.get<IItem[]>('/assets/data/items.json'); 
+  }
   getData(): Observable<any> {
-    return this.http.get<any>('./data/items.json');
+    return this.http.get<any>('/assets/data/items.json');
   }
 }
