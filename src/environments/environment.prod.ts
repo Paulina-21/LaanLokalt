60477a66e8d2484cb53c8541b13a307079f4a569
Niko D <nikodojan@live.de>
2023-04-17 10:20:59 +0200

    added firebase to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAUiPwGkvoqEOGd8eXF8Q_0tqFUXVYaJH0",
+    authDomain: "bob-tours-nd0619.firebaseapp.com",
+    databaseURL: "https://bob-tours-nd0619-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "bob-tours-nd0619",
+    storageBucket: "bob-tours-nd0619.appspot.com",
+    messagingSenderId: "230376163831",
+    appId: "1:230376163831:web:57c5d995594dd668de46f2",
+    measurementId: "G-ZLKWWCDCKS"
+  }
 };
